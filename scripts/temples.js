19e2f1b1fc8b39195ed1eeef6bd6d7b663260c21
owenmeredith12 <owen.meredith1516@gmail.com>
2025-05-16 11:03:07 -0600

    pushing just in case

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const modified = document.getElementById("lastModified");
+    if (modified) {
+        modified.textContent = "Last modified: " + document.lastModified;
+    }
+});
+
